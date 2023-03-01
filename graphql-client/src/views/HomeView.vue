f1ab1439ef2abd84357c958fc3a3ceaed971441c
Drew Miller <d.m.lespaul@gmail.com>
2023-02-28 18:47:41 -0800

    added library view to main

--- graphql-client/src/views/HomeView.vue
+++ graphql-client/src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <script lang="ts">
 import TheSchool from "@/components/SchoolComponent.vue";
+import TheLibrary from "@/components/LibraryComponent.vue";
 
 export default {
-  components: { TheSchool }
+  components: { TheSchool, TheLibrary }
 }
 </script>
 
@@ -10,6 +11,7 @@ export default {
   <div class="container">
     <h1 class="header">GraphQL Vue Client</h1>
     <TheSchool ></TheSchool>
+    <TheLibrary></TheLibrary>
   </div>
 </template>
 
